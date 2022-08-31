35b29bc2b1b4cb1c2bf5835b4b3e663c1763a6f2
Dev-jwJeong <rgn95652@gmail.com>
2022-08-31 17:35:07 +0900

    Feat : 토큰 유무에 따라서 리다이렉트

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import TodoPage from './pages/TodoPage';
 
+let isLogin = localStorage.getItem('token');
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />
+      <Route
+        path="/"
+        element={isLogin ? <Navigate replace to="/todo" /> : <LoginPage />}
+      />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/todo" element={<TodoPage />} />
     </Routes>
