17d4aa7c6660a5c607e0a56a1d722043eb64fe21
[SW2/서울/2반]김한얼 <gksdjf80@naver.com>
2022-09-01 08:40:41 +0900

    Feat axios middleware

--- src/api/todoApi.js
+++ src/api/todoApi.js
@@ -1,65 +1,77 @@
-import axios from 'axios';
-import { GetTokenInStorage } from '../utils/Localstorage';
+import { instance } from './customAxios';
 
-const BASE_URL = process.env.REACT_APP_API_URL;
+const COMMON_URL = process.env.REACT_APP_API_URL + 'todos';
 
-export class todoAPI {
-  TodoApi;
-  constructor() {
-    this.TodoApi = axios.create({
-      baseURL: BASE_URL,
-      headers: {
-        Authorization: `Bearer ${GetTokenInStorage()}`,
-      },
+export const createTodo = async todo => {
+  try {
+    const response = await instance.post(COMMON_URL, {
+      todo,
     });
+    return response;
+  } catch (e) {
+    throw e;
   }
-
-  createTodo = async todo => {
-    const { data } = await this.TodoApi.post('/todos', todo);
-    return data;
-  };
-
-  getTodos = async () => {
-    const { data } = await this.TodoApi.get('/todos');
-    return data;
-  };
-
-  updateTodo = async (id, todo, isCompleted) => {
-    const { data } = await this.TodoApi.put(`/todos/${id}`, {
+};
+export const getTodos = async () => {
+  try {
+    const response = await instance.get(COMMON_URL);
+    return response;
+  } catch (e) {
+    throw e;
+  }
+};
+export const updateTodo = async (todo, isCompleted, id) => {
+  try {
+    const response = await instance.put(COMMON_URL + `/${id}`, {
       todo,
       isCompleted,
     });
-    return data;
-  };
-
-  deleteTodo = async id => {
-    const data = await this.TodoApi.delete(`/todos/${id}`);
-    return data.status;
-  };
-}
-
-const Todoapi = axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    Authorization: `Bearer ${GetTokenInStorage()}`,
-  },
-});
-
-export const createTodo = async todo => {
-  const { data } = await Todoapi.post('/todos', todo);
-  return data;
-};
-export const getTodos = async () => {
-  const { data } = await Todoapi.get('/todos');
-  return data;
-};
-
-export const updateTodo = async (id, todo, isCompleted) => {
-  const { data } = await Todoapi.put(`/todos/${id}`, { todo, isCompleted });
-  return data;
+    return response;
+  } catch (e) {
+    throw e;
+  }
 };
 
 export const deleteTodo = async id => {
-  const data = await Todoapi.delete(`/todos/${id}`);
-  return data.status;
+  try {
+    const response = await instance.delete(COMMON_URL + `/${id}`);
+    return response;
+  } catch (e) {
+    throw e;
+  }
 };
+
+// export class todoAPI {
+//   TodoApi;
+//   constructor() {
+//     this.TodoApi = axios.create({
+//       baseURL: BASE_URL,
+//       headers: {
+//         Authorization: `Bearer ${GetTokenInStorage()}`,
+//       },
+//     });
+//   }
+
+//   createTodo = async todo => {
+//     const { data } = await this.TodoApi.post('/todos', todo);
+//     return data;
+//   };
+
+//   getTodos = async () => {
+//     const { data } = await this.TodoApi.get('/todos');
+//     return data;
+//   };
+
+//   updateTodo = async (id, todo, isCompleted) => {
+//     const { data } = await this.TodoApi.put(`/todos/${id}`, {
+//       todo,
+//       isCompleted,
+//     });
+//     return data;
+//   };
+
+//   deleteTodo = async id => {
+//     const data = await this.TodoApi.delete(`/todos/${id}`);
+//     return data.status;
+//   };
+// }
