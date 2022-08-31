6b7f854c21a04e2d7f6d36158733590954537316
cos4338@gmail.como <seungyeonchoo@chuseung-yeon-ui-MacBookAir.local>
2022-08-31 20:56:13 +0900

    Chore: module update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import AuthForm from './component/AuthForm';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import GlobalStyle from './style/GlobalStyle';
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <AuthForm />
+    </>
+  );
 }
 
 export default App;
