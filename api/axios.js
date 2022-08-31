df7c66d57be82021f2c3c8397c9874f3bb8f9f96
cos4338@gmail.como <seungyeonchoo@chuseung-yeon-ui-MacBookAir.local>
2022-08-31 20:55:38 +0900

    Fix: fix api url

--- api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = process.env.REACT_PRODUCT_BASE_URL;
-
-export const instance = axios.create({
-  baseURL: BASE_URL,
-  headers: { 'Content-Type': 'application/json' },
-});
