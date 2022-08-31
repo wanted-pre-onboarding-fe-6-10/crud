c3235297653312dd74de9c64dbbbaa275c913a6c
eazae <xynezae@gmail.com>
2022-08-31 18:37:22 +0900

    Feat: axios instance and api configurations

---
+++ src/api/axios.js
@@ -0,0 +1,31 @@
+import { default as axios } from 'axios';
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+  headers: {
+    'Content-type': 'application/json',
+    'Access-Control-Allow-Credentials': true,
+  },
+});
+
+/* Apply Interceptor */
+// HTTP request interceptor
+instance.interceptors.request.use(
+  config => {
+    const accessToken = localStorage.getItem('access_token');
+    if (accessToken) {
+      config.headers.Authorization = 'Bearer ' + accessToken;
+    }
+    return config;
+  },
+  err => {
+    return Promise.reject(err);
+  }
+);
+
+instance.interceptors.response.use(
+  response => response,
+  err => Promise.resolve(err.response)
+);
+
+export default instance;
