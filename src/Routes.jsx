93b5c1f5fbfb7854d64762a279ba63eeaaf2ec6b
eazae <xynezae@gmail.com>
2022-08-31 19:15:25 +0900

    Feat: routing logic change

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,19 +1,18 @@
 import { Routes as ReactRoutes, Route, Navigate } from 'react-router-dom';
-import Login from './Login';
-import Todo from './Todo/Todo';
+import { MainPage, TodoPage } from './pages';
 
 function Routes() {
   const isToken = localStorage.getItem('accessToken');
   return (
     <ReactRoutes>
-      <Route
-        path="/"
-        element={!isToken ? <Login /> : <Navigate replace to="/todo" />}
-      />
+      <Route path="/login" element={<MainPage />} />
+      <Route path="/signup" element={<MainPage />} />
       <Route
         path="/todo"
-        element={isToken ? <Todo /> : <Navigate replace to="/" />}
+        element={isToken ? <TodoPage /> : <Navigate replace to="/" />}
       />
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </ReactRoutes>
   );
 }
